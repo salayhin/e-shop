fca145098a2c65c461fc8ab90ed81a2c33cd718d
Sirajus Salayhin <salayhin@gmail.com>
2013-12-20 01:18:15 +0600

    Rails 3.2.16 added.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EShop::Application.config.secret_token = '2a1633f52b39c3a5d7de65a6c22a7b024d289aed2978c9528d4b750de345bc4acf52ed9fb274768dde14a568335f8163d9cabde934e989b0e286e7a9225e9c37'
